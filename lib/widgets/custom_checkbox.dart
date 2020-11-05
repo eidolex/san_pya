8309340ada07cdc7718d826b9684bd6242edea3f
Si Thu Hpyo <sandaraion@gmail.com>
2020-11-05 15:52:56 +0630

    add custom_checkbox

---
+++ lib/widgets/custom_checkbox.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+class CustomCheckbox extends StatefulWidget {
+  final String label;
+
+  final double size;
+
+  final double spacing;
+
+  final TextStyle labelStyle;
+
+  final Color color;
+
+  final Color inactiveColor;
+
+  CustomCheckbox({
+    Key key,
+    this.label,
+    this.size = 18,
+    this.spacing = 8,
+    this.labelStyle,
+    this.color,
+    this.inactiveColor,
+  }) : super(key: key);
+
+  @override
+  _CustomCheckboxState createState() => _CustomCheckboxState();
+}
+
+class _CustomCheckboxState extends State<CustomCheckbox> {
+  bool _isSelected = false;
+
+  @override
+  Widget build(BuildContext context) {
+    var color = widget.color ?? Theme.of(context).primaryColor;
+    var size = widget.size;
+    var label = widget.label;
+    var hasLabel = widget.label != null;
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          _isSelected = !_isSelected;
+        });
+      },
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          if (hasLabel) ceateLabel(label),
+          Container(
+            margin: EdgeInsets.only(left: hasLabel ? widget.spacing : 0),
+            decoration: BoxDecoration(
+                color: _isSelected ? color : widget.inactiveColor,
+                border: Border.all(
+                    width: 1, color: _isSelected ? color : Color(0xFFE5E5E5))),
+            width: size,
+            height: size,
+            child: _isSelected
+                ? Icon(
+                    Icons.check,
+                    color: Colors.white,
+                    size: size - 2,
+                  )
+                : null,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Text ceateLabel(String text) {
+    return Text(
+      text,
+      style: widget.labelStyle ??
+          TextStyle(
+            fontWeight: FontWeight.w300,
+            fontSize: 12,
+          ),
+    );
+  }
+}
