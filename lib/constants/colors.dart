cb616d7173b5d9f3fab384f918d3c3f668ffd715
Si Thu Hpyo <sandaraion@gmail.com>
2020-11-06 17:53:36 +0630

    add private constructor

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -2,21 +2,28 @@ import 'package:flutter/material.dart';
 
 // Main colors
 class MainColors {
+  MainColors._();
+
   static Color primary = Color(0xFFE63A3F);
   static Color accent = Color(0xFFF0BC30);
 }
 
 // Body Text colors
 class TextColors {
+  TextColors._();
   static Color bodyText = Color(0xFF4E4E4E);
 }
 
 // Input border colors
 class InputBorderColors {
+  InputBorderColors._();
+
   static Color textInput = Color(0xFFE5E5E5);
   static Color btn = Color(0xFF1D1D1D);
 }
 
 class BoxBorderColors {
+  BoxBorderColors._();
+
   static Color primary = Color(0xFFEBEBEB);
 }
