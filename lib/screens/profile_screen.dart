b2f9f52292cfebae25f8a0e1c663b7afe3665bb0
Si Thu Hpyo <sandaraion@gmail.com>
2020-11-06 18:58:54 +0630

    add profile screen

---
+++ lib/screens/profile_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:san_pya/widgets/app_bar.dart';
+
+class ProfileScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: appBar(context, "Profile"),
+    );
+  }
+}
