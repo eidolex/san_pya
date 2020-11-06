f0303a0f3dd5a31d1fa5abc74441b7b126e20b05
Si Thu Hpyo <sandaraion@gmail.com>
2020-11-06 19:02:30 +0630

    make basic design of shopping_cart_screen

--- lib/screens/shopping_cart_screen.dart
+++ lib/screens/shopping_cart_screen.dart
@@ -1,8 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:san_pya/widgets/app_bar.dart';
 
 class ShoppingCartScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: appBar(context, "Shopping Cart"),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            margin: EdgeInsets.fromLTRB(16, 16, 16, 4),
+            child: Text(
+              "Your Cart",
+              style: TextStyle(fontWeight: FontWeight.w600, fontSize: 18),
+            ),
+          ),
+          Container(
+              margin: EdgeInsets.fromLTRB(16, 0, 16, 16),
+              child: Text(
+                "You have 5 items in your cart",
+                style: TextStyle(fontSize: 10),
+              )),
+        ],
+      ),
+    );
   }
 }
