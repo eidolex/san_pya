5e5bafef643cfda5713682bb1eff84ce508ba88a
Si Thu Hpyo <sandaraion@gmail.com>
2020-11-06 17:07:35 +0630

    add block button

---
+++ lib/widgets/block_button.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class BlockButton extends StatelessWidget {
+  const BlockButton({
+    Key key,
+    @required this.text,
+    @required this.onPressed,
+    this.icon,
+    this.color,
+  }) : super(key: key);
+
+  final String text;
+
+  final Color color;
+
+  final IconData icon;
+
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    var color = this.color ?? Theme.of(context).accentColor;
+    var flatButton = FlatButton(
+      color: color,
+      textColor: Colors.white,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(6)),
+      padding: EdgeInsets.only(top: 16, bottom: 16),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          if (this.icon != null)
+            Container(
+              child: Icon(
+                icon,
+                size: 20,
+              ),
+              margin: EdgeInsets.only(right: 14),
+            ),
+          Text(this.text)
+        ],
+      ),
+      onPressed: this.onPressed,
+    );
+    return SizedBox(
+      width: double.infinity,
+      child: flatButton,
+    );
+  }
+}
