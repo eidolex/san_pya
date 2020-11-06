6d0940ff5646f42d0b01d2965673c90028dfa13c
pyaehlianmoe <pyae.phm@gmail.com>
2020-11-07 01:34:49 +0800

    fix button stuck in bottom

--- lib/utils/fullscreen_height.dart
+++ lib/utils/fullscreen_height.dart
@@ -5,11 +5,12 @@ double fullScreenHeight(BuildContext context) {
   var query = MediaQuery.of(context);
   var height = query.size.height;
   var padding = query.padding;
+  var bottom = query.viewInsets.bottom;
 
   if (isIOS()) {
     // height without SafeArea
-    return height - padding.top - padding.bottom;
+    return height - bottom - padding.top - padding.bottom;
   }
 
-  return height - padding.top - kToolbarHeight;
+  return height - bottom - padding.top - kToolbarHeight;
 }
