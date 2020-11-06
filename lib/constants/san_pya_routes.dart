53913562832a034f18c6a519fdca84fec8ce9cc8
Si Thu Hpyo <sandaraion@gmail.com>
2020-11-06 17:08:16 +0630

    add route constant

---
+++ lib/constants/san_pya_routes.dart
@@ -0,0 +1,7 @@
+class SanPyaRoutes {
+  SanPyaRoutes._();
+
+  static const accountSetting = '/account_setting';
+  static const productDetail = '/product_detail';
+  static const shoppingCart = '/shopping_cart';
+}
