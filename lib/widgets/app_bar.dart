093f613cb9c2b89309ee5eb1ec34c569c5eb4067
Si Thu Hpyo <sandaraion@gmail.com>
2020-11-07 13:52:30 +0630

    add appBarCustomTitle

--- lib/widgets/app_bar.dart
+++ lib/widgets/app_bar.dart
@@ -3,34 +3,52 @@ import 'package:san_pya/constants/san_pya_routes.dart';
 import 'package:san_pya/constants/san_pya_icons.dart';
 
 Widget appBar(BuildContext context, String title) {
-  var canGoBack = Navigator.canPop(context);
-  var currentRoute = ModalRoute.of(context).settings.name;
-  var showCart = currentRoute != SanPyaRoutes.shoppingCart;
   return AppBar(
     title: Text(
       title,
       style: TextStyle(fontWeight: FontWeight.w600),
     ),
-    actions: [
-      if (showCart)
-        IconButton(
-            icon: Icon(SanPyaIcons.cart, size: 20),
-            onPressed: () {
-              Navigator.pushNamed(context, SanPyaRoutes.shoppingCart);
-            })
-    ],
-    leading: canGoBack
-        ? IconButton(
-            icon: Icon(SanPyaIcons.left_arrow, size: 16),
-            onPressed: () {
-              Navigator.pop(context);
-            })
-        : IconButton(
-            icon: Icon(SanPyaIcons.user, size: 20),
-            onPressed: () {
-              Navigator.pushNamed(context, SanPyaRoutes.profile);
-            }),
+    actions: [_buildCartButton(context)],
+    leading: _buildLeadingButton(context),
     centerTitle: true,
     elevation: 0,
   );
+}
+
+Widget appBarCustomTitle(BuildContext context, Widget title) {
+  return AppBar(
+    title: title,
+    actions: [_buildCartButton(context)],
+    leading: _buildLeadingButton(context),
+    centerTitle: true,
+    elevation: 0,
+  );
+}
+
+Widget _buildCartButton(BuildContext context) {
+  var currentRoute = ModalRoute.of(context).settings.name;
+  var showCart = currentRoute != SanPyaRoutes.shoppingCart;
+
+  return showCart
+      ? IconButton(
+          icon: Icon(SanPyaIcons.cart, size: 20),
+          onPressed: () {
+            Navigator.pushNamed(context, SanPyaRoutes.shoppingCart);
+          })
+      : null;
+}
+
+Widget _buildLeadingButton(BuildContext context) {
+  var canGoBack = Navigator.canPop(context);
+  return canGoBack
+      ? IconButton(
+          icon: Icon(SanPyaIcons.left_arrow, size: 16),
+          onPressed: () {
+            Navigator.pop(context);
+          })
+      : IconButton(
+          icon: Icon(SanPyaIcons.user, size: 20),
+          onPressed: () {
+            Navigator.pushNamed(context, SanPyaRoutes.profile);
+          });
 }
