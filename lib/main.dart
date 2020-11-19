08455e1cdd637385915965485084fbcc5d76265f
Si Thu Hpyo <sandaraion@gmail.com>
2020-11-19 15:43:50 +0630

    remove unused import

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:san_pya/bloc/cart_bloc.dart';
-import 'package:san_pya/bloc/order_bloc.dart';
-import 'package:san_pya/bloc/product_bloc.dart';
 import 'package:san_pya/constants/san_pya_fonts.dart';
 import 'package:san_pya/constants/san_pya_routes.dart';
 import 'package:san_pya/repository/order_repository.dart';
 import 'package:san_pya/repository/product_repository.dart';
 import 'package:san_pya/router.dart';
-import 'package:san_pya/screens/language_setting_screen.dart';
-import 'package:san_pya/screens/login_screen.dart';
-import 'package:san_pya/screens/main_screen.dart';
-import 'package:san_pya/screens/notification_setting_screen.dart';
-import 'package:san_pya/screens/order_detail_screen.dart';
-import 'package:san_pya/screens/order_history_screen.dart';
-import 'package:san_pya/screens/password_setting_screen.dart';
-import 'package:san_pya/screens/product_detail_screen.dart';
 import 'package:san_pya/constants/colors.dart';
-import 'package:san_pya/screens/profile_screen.dart';
-import 'package:san_pya/screens/shopping_cart_screen.dart';
 
 void main() {
   runApp(MyApp(
